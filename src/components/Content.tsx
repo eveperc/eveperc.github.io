8cf12f48435bcf3f9dcf3c7fb6059ce2436034af
eveperc <eve.perc@icloud.com>
2023-03-28 18:35:48 +0900

    add newt

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,41 +1,21 @@
-import { createStyles, SimpleGrid, Card, Image, Text, Container, AspectRatio } from '@mantine/core';
+import { SimpleGrid, Card, Image, Text, Container, AspectRatio } from '@mantine/core';
+import { newtClient,Article } from "../lib/newt";
 
-const mockdata = [
-  {
-    title: 'Top 10 places to visit in Norway this summer',
-    image:
-      'https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
-    date: 'August 18, 2022',
+const { items: articles } = await newtClient.getContents<Article>({
+  appUid: "blog",
+  modelUid: "article",
+  query: {
+    select: ["title", "slug","body"],
   },
-  {
-    title: 'Best forests to visit in North America',
-    image:
-      'https://images.unsplash.com/photo-1448375240586-882707db888b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
-    date: 'August 27, 2022',
-  },
-  {
-    title: 'Hawaii beaches review: better than you think',
-    image:
-      'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
-    date: 'September 9, 2022',
-  },
-  {
-    title: 'Mountains at night: 12 best locations to enjoy the view',
-    image:
-      'https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
-    date: 'September 12, 2022',
-  },
-];
+});
 
 export function MyContent() {
 
-  const cards = mockdata.map((article) => (
-    <Card key={article.title} p="md" radius="md" component="a" href="#" className="px-0.5">
+  const cards = articles.map((article) => (
+    <Card key={article.title} p="md" radius="md" component="a" href={`/articles/${article.slug}`} className="px-0.5">
       <AspectRatio ratio={1920 / 1080}>
-        <Image src={article.image} />
       </AspectRatio>
       <Text color="dimmed" size="xs" transform="uppercase" weight={700} mt="md">
-        {article.date}
       </Text>
       <Text mt={5}>
         {article.title}
---
+++ src/lib/newt.ts
@@ -0,0 +1,18 @@
+import {createClient} from 'newt-client-js'
+
+export interface Article {
+  title:string
+  slug:string
+  body:string
+  categories:[
+    {
+      _id:string
+    }
+  ];
+}
+
+export const newtClient = createClient({
+  spaceUid: import.meta.env.NEWT_SPACE_UID,
+  token:import.meta.env.NEWT_CDN_API_TOKEN,
+  apiType: 'cdn',
+})
