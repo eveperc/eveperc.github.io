a8e5067d35860cfcaae334da7857d0fb0f92b28a
eveperc <eve.perc@icloud.com>
2025-01-12 18:10:44 +0900

    config修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import { defineConfig } from 'astro/config';
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
-import partytown from "@astrojs/partytown";
+import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://eveperc.github.io',
-  integrations: [react(), tailwind(), partytown({
-    // Example: Add dataLayer.push as a forwarding-event.
-    config: {
-      forward: ["dataLayer.push"]
-    }
-  })]
+  integrations: [
+    tailwind({
+      // TailwindのJITモードを有効化
+      applyBaseStyles: false,
+      // DaisyUIの設定を含める
+      config: {
+        plugins: [require("daisyui")]
+      }
+    })
+  ]
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [require('daisyui')],
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  // DaisyUIのテーマ設定
+  daisyui: {
+    themes: ["light", "dark"], // 必要なテーマを指定
+    darkTheme: "dark",
+    base: true,
+    styled: true,
+    utils: true,
+  }
 }
