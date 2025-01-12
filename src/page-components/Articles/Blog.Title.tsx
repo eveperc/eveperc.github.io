5d985086b6f7afcec29b40542a0ed865f83c027b
eveperc <eve.perc@icloud.com>
2025-01-12 16:52:36 +0900

    articleの削除

--- src/page-components/Articles/Blog.Title.tsx
+++
@@ -1,9 +0,0 @@
-export type BlogTitleProps = {
-  children: React.ReactNode;
-};
-
-export default function({ children }: BlogTitleProps) {
-  return (
-    <h2 className="p-5 text-center font-bold">{children}</h2>
-  )
-}
--- src/page-components/Articles/Blog.UpdatedAt.tsx
+++
@@ -1,9 +0,0 @@
-export type BlogUpdatedAtProps = {
-  children: React.ReactNode;
-};
-
-export default function({ children }: BlogUpdatedAtProps) {
-  return (
-    <div className="p-3 text-3xl font-bold">{children}</div>
-  )
-}
